e2fcc8a16bac65bb7280357b7a82b5022e459cf8
eduard.levchenko@arcelormittal.com <eduard.levchenko@arcelormittal.com>
2020-12-27 21:28:24 +0200

    1

--- sql/Select/test fn get_value_tank1_of_interval.sql
+++ sql/Select/test fn get_value_tank1_of_interval.sql
@@ -1,7 +1,7 @@
 use [BENZINEtanks]
-declare @start datetime = CONVERT(datetime, '2020-11-26 14:30:19.447' ,120)
+declare @start datetime = CONVERT(datetime, '2020-11-26 00:00:00' ,120)
 
-declare @stop datetime = CONVERT(datetime, '2020-11-26 14:30:39.390' ,120)
+declare @stop datetime = CONVERT(datetime, '2020-11-26 14:30:39' ,120)
 
 select * from [dbo].[get_value_tank1_of_interval](@start, @stop) order by dt
 select * from [dbo].[get_value_tank2_of_interval](@start, @stop) order by dt
@@ -9,4 +9,16 @@ select * from [dbo].[get_value_tank2_of_interval](@start, @stop) order by dt
 select * from [dbo].[get_difference_value_tank1_of_interval](@start, @stop)
 select * from [dbo].[get_difference_value_tank2_of_interval](@start, @stop)
 select * from [dbo].[get_difference_value_tanks_of_interval](@start, @stop)
+--select * from [dbo].[get_remains_tank1](@stop)
+--select * from [dbo].[get_remains_tank2](@stop)
+--select * from [dbo].[get_remains_tanks](@stop)
 
+
+go
+
+
+--use [BENZINEweb]
+--declare @start datetime = CONVERT(datetime, '2020-11-24 00:00:00' ,120)
+--declare @stop datetime = CONVERT(datetime, '2020-11-24 23:59:59' ,120)
+
+--select * from [dbo].[get_sales_of_interval](@start, @stop)
