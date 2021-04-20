95075b0793bac3444a1ec37229dd0383a78252d2
Levchenko <lev75.eduard@gmail.com>
2021-04-20 23:16:37 +0300

    report services

---
+++ ReportBenzeneServices/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("ReportBenzeneServices")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReportBenzeneServices")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// компонент COM, задайте для атрибута ComVisible этого типа значение TRUE.
+[assembly: ComVisible(false)]
+
+// Указанный ниже идентификатор GUID предназначен для идентификации библиотеки типов, если этот проект будет видимым для COM-объектов
+[assembly: Guid("dfe65c25-df62-4217-b335-4be19b2cde48")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ReportBenzeneServices/ReportService.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using MessageLog;
+
+namespace ReportBenzeneServices
+{
+    public partial class ReportService : ServiceBase
+    {
+        System.Timers.Timer timer_services = new System.Timers.Timer();
+
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        private bool[] jobs = new bool[24] { false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false };
+
+        private bool time_daily = false;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public long dwServiceType;
+            public ServiceState dwCurrentState;
+            public long dwControlsAccepted;
+            public long dwWin32ExitCode;
+            public long dwServiceSpecificExitCode;
+            public long dwCheckPoint;
+            public long dwWaitHint;
+        };
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+
+        public ReportService()
+        {
+            InitializeComponent();
+            //// Настроем таймер контроля выполнения сервиса
+            timer_services.Interval = 1000 * 60; // 1 раз в минуту
+            timer_services.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimerServices);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Update the service state to Start Pending.
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            serviceStatus.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+            // Запустить таймер контроля сервиса
+            timer_services.Start();
+            String.Format("Сервис ReportBenzeneServices - старт").SaveInformation();
+
+            // Update the service state to Running.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        protected override void OnStop()
+        {
+            timer_services.Stop();
+            String.Format("Сервис ReportBenzeneServices - стоп").SaveInformation();
+        }
+
+        public void OnTimerServices(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            try
+            {
+                String.Format("Сервис ReportBenzeneServices - сработал таймер OnTimerServices").SaveWarning();
+                //EFDaily_Report efdl = new EFDaily_Report();
+                //EFDaily_Accounting_Report ef_dar = new EFDaily_Accounting_Report();
+                //EFDaily_Accounting_Detali_Report ef_adr = new EFDaily_Accounting_Detali_Report();
+                //Transfer tr = new Transfer();
+                //EFTanksLog ef_tl = new EFTanksLog();
+
+                string log_mes;
+                DateTime dt = DateTime.Now;
+                int h = dt.Hour;
+                int m = dt.Minute;
+                //  В 0:5 сбосить тригеры отработки заданий
+                if (h == 0 && (m >= 5 && m <= 6))
+                {
+                    // обнулим события
+                    for (int i = 0; i < 24; i++)
+                    {
+                        jobs[i] = false;
+                    }
+                    time_daily = false;
+                }
+                // Сработка события каждый час
+                if (!jobs[h] && (m >= 58 && m <= 59))
+                {
+                    jobs[h] = true;
+                    log_mes = String.Format("Сервис ReportBenzeneServices - сработал таймер на {0} часов", h);
+                    log_mes.SaveWarning();
+                    //log_mes.SaveInformation();
+                    //trk_log.AddTRKLogs(new TRKLogs()
+                    //{
+                    //    ID = 0,
+                    //    DateTime = DateTime.Now,
+                    //    Level = 4,
+                    //    UserName = "ReportTRKServeces",
+                    //    Log = log_mes
+                    //});
+                    //addCounters();
+                }
+
+                if (h == 1 && (m >= 5 && m <= 6) && !time_daily)
+                {
+                    String.Format("Сервис ReportBenzeneServices - сработал таймер на 0 часов").SaveInformation();
+
+                    //int res = 0;
+                    // Суточный с пересчетом к 15 градусам
+                    //res = ef_dar.AddDailyAccountingReport();
+                    //log_mes = String.Format("Сервис ReportTRKServices - Отработал метод AddDaily_Accounting_Report - Код выполнения:{0}", res);
+                    //log_mes.SaveInformation();
+                    //trk_log.AddTRKLogs(new TRKLogs()
+                    //{
+                    //    ID = 0,
+                    //    DateTime = DateTime.Now,
+                    //    Level = 4,
+                    //    UserName = "ReportTRKServeces",
+                    //    Log = log_mes
+                    //});
+                    //// Суточный детально по емкостям
+                    //res = ef_adr.AddDailyAccountingDetaliReport();
+                    //log_mes = String.Format("Сервис ReportTRKServices - Отработал метод AddDaily_Accounting_Detali_Report - Код выполнения:{0}", res);
+                    //log_mes.SaveInformation();
+                    //trk_log.AddTRKLogs(new TRKLogs()
+                    //{
+                    //    ID = 0,
+                    //    DateTime = DateTime.Now,
+                    //    Level = 4,
+                    //    UserName = "ReportTRKServeces",
+                    //    Log = log_mes
+                    //});
+
+                    //// Суточный с переносом в ЦОД
+                    //int res_rt = tr.RemainsTanksToDC();
+                    //int res_rect = tr.ReceivingTanksToDC();
+                    //int res_dt = tr.DeliveryTanksToDC();
+                    //int res_radr = tr.Daily_Accounting_Detali_ReportToDC();
+                    //int res_dar = tr.Daily_Accounting_ReportToDC();
+
+                    //log_mes = String.Format("Сервис ReportTRKServices - Отработали методы переноса в ЦОД - Коды выполнения RemainsTanksToDC:{0}, ReceivingTanksToDC:{1}, DeliveryTanksToDC:{2}, Daily_Accounting_Detali_ReportToDC:{3}, Daily_Accounting_ReportToDC:{4}",
+                    //    res_rt, res_rect, res_dt, res_radr, res_dar);
+                    //log_mes.SaveInformation();
+                    //trk_log.AddTRKLogs(new TRKLogs()
+                    //{
+                    //    ID = 0,
+                    //    DateTime = DateTime.Now,
+                    //    Level = 4,
+                    //    UserName = "ReportTRKServeces",
+                    //    Log = log_mes
+                    //});
+                    //// Чистка базы
+                    //res = ef_tl.Delete_Tanks();
+                    //log_mes = String.Format("Сервис ReportTRKServices - Отработал метод Delete_Tanks - Код выполнения:{0}", res);
+                    //log_mes.SaveInformation();
+                    //trk_log.AddTRKLogs(new TRKLogs()
+                    //{
+                    //    ID = 0,
+                    //    DateTime = DateTime.Now,
+                    //    Level = 4,
+                    //    UserName = "ReportTRKServeces",
+                    //    Log = log_mes
+                    //});
+                    time_daily = true;
+                }
+                //if (h == 6 && (m >= 58 && m <= 59) && !time_sm_day)
+                //{
+                //    log_mes = String.Format("Сервис ReportTRKServices - сработал таймер на 7 часов");
+                //    log_mes.SaveInformation();
+                //    trk_log.AddTRKLogs(new TRKLogs()
+                //    {
+                //        ID = 0,
+                //        DateTime = DateTime.Now,
+                //        Level = 4,
+                //        UserName = "ReportTRKServeces",
+                //        Log = log_mes
+                //    });
+                //    addCounters();
+                //    time_sm_day = true;
+                //}
+                //if (h == 18 && (m >= 58 && m <= 59) && !time_sm_night)
+                //{
+                //    log_mes = String.Format("Сервис ReportTRKServices - сработал таймер на 19 часов");
+                //    log_mes.SaveInformation();
+                //    trk_log.AddTRKLogs(new TRKLogs()
+                //    {
+                //        ID = 0,
+                //        DateTime = DateTime.Now,
+                //        Level = 4,
+                //        UserName = "ReportTRKServeces",
+                //        Log = log_mes
+                //    });
+                //    addCounters();
+                //    time_sm_night = true;
+                //}
+                //if (h == 23 && (time_daily || time_sm_day || time_sm_night))
+                //{
+                //    time_daily = false;
+                //    //time_sm_day = false;
+                //    //time_sm_night = false;
+                //}
+            }
+            catch (Exception e)
+            {
+                string log_mes = String.Format("OnTimerServices(sender={0}, args={1})", sender, args.ToString());
+                log_mes.SaveError(e);
+                //trk_log.AddTRKLogs(new TRKLogs()
+                //{
+                //    ID = 0,
+                //    DateTime = DateTime.Now,
+                //    Level = 2,
+                //    UserName = "ReportTRKServeces",
+                //    Log = log_mes
+                //});
+            }
+        }
+    }
+}
