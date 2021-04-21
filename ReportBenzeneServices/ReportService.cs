b159dfddb8b30ea98e3dfabaa3a5d963cee0bf39
Levchenko <lev75.eduard@gmail.com>
2021-04-21 22:57:41 +0300

    add report daily

--- ReportBenzeneServices/ReportService.cs
+++ ReportBenzeneServices/ReportService.cs
@@ -8,6 +8,7 @@ using System.Runtime.InteropServices;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using EFBenzene.Concrete;
 using MessageLog;
 
 namespace ReportBenzeneServices
@@ -80,19 +81,13 @@ namespace ReportBenzeneServices
         {
             try
             {
-                String.Format("Сервис ReportBenzeneServices - сработал таймер OnTimerServices").SaveWarning();
-                //EFDaily_Report efdl = new EFDaily_Report();
-                //EFDaily_Accounting_Report ef_dar = new EFDaily_Accounting_Report();
-                //EFDaily_Accounting_Detali_Report ef_adr = new EFDaily_Accounting_Detali_Report();
-                //Transfer tr = new Transfer();
-                //EFTanksLog ef_tl = new EFTanksLog();
-
+                //String.Format("Сервис ReportBenzeneServices - сработал таймер OnTimerServices").SaveWarning();
                 string log_mes;
                 DateTime dt = DateTime.Now;
                 int h = dt.Hour;
                 int m = dt.Minute;
                 //  В 0:5 сбосить тригеры отработки заданий
-                if (h == 0 && (m >= 5 && m <= 6))
+                if (h == 0 && (m >= 0 && m <= 2))
                 {
                     // обнулим события
                     for (int i = 0; i < 24; i++)
@@ -100,87 +95,93 @@ namespace ReportBenzeneServices
                         jobs[i] = false;
                     }
                     time_daily = false;
+                    String.Format("Сервис ReportBenzeneServices, тригеры отработки заданий - сброшены.").SaveWarning();
                 }
                 // Сработка события каждый час
-                if (!jobs[h] && (m >= 58 && m <= 59))
+                if (!jobs[h] && (m >= 5 && m <= 6))
                 {
                     jobs[h] = true;
                     log_mes = String.Format("Сервис ReportBenzeneServices - сработал таймер на {0} часов", h);
                     log_mes.SaveWarning();
-                    //log_mes.SaveInformation();
-                    //trk_log.AddTRKLogs(new TRKLogs()
-                    //{
-                    //    ID = 0,
-                    //    DateTime = DateTime.Now,
-                    //    Level = 4,
-                    //    UserName = "ReportTRKServeces",
-                    //    Log = log_mes
-                    //});
-                    //addCounters();
+
+                    if (h == 0) {
+                        // Выполнить ХП перенса суточного отчета
+                        EFWEBDbContext ef_web = new EFWEBDbContext();
+                        try
+                        {
+                            string sql = "EXEC [dbo].[Transfer_Daily_Report_Benzene]";
+                            int res = ef_web.Database.ExecuteSqlCommand(sql);
+                            String.Format("ХП [dbo].[Transfer_Daily_Report_Benzene] - выполнена, перенесено {0} строк", res).SaveWarning();
+                        }
+                        catch (Exception e)
+                        {
+                            String.Format("Ошибка выполнения ХП [dbo].[Transfer_Daily_Report_Benzene]").SaveError(e);
+                        }
+                    }
                 }
 
-                if (h == 1 && (m >= 5 && m <= 6) && !time_daily)
-                {
-                    String.Format("Сервис ReportBenzeneServices - сработал таймер на 0 часов").SaveInformation();
+                //if (h == 1 && (m >= 5 && m <= 6) && !time_daily)
+                //{
+                //    String.Format("Сервис ReportBenzeneServices - сработал таймер на 0 часов").SaveInformation();
 
-                    //int res = 0;
-                    // Суточный с пересчетом к 15 градусам
-                    //res = ef_dar.AddDailyAccountingReport();
-                    //log_mes = String.Format("Сервис ReportTRKServices - Отработал метод AddDaily_Accounting_Report - Код выполнения:{0}", res);
-                    //log_mes.SaveInformation();
-                    //trk_log.AddTRKLogs(new TRKLogs()
-                    //{
-                    //    ID = 0,
-                    //    DateTime = DateTime.Now,
-                    //    Level = 4,
-                    //    UserName = "ReportTRKServeces",
-                    //    Log = log_mes
-                    //});
-                    //// Суточный детально по емкостям
-                    //res = ef_adr.AddDailyAccountingDetaliReport();
-                    //log_mes = String.Format("Сервис ReportTRKServices - Отработал метод AddDaily_Accounting_Detali_Report - Код выполнения:{0}", res);
-                    //log_mes.SaveInformation();
-                    //trk_log.AddTRKLogs(new TRKLogs()
-                    //{
-                    //    ID = 0,
-                    //    DateTime = DateTime.Now,
-                    //    Level = 4,
-                    //    UserName = "ReportTRKServeces",
-                    //    Log = log_mes
-                    //});
+                //    //int res = 0;
+                //    // Суточный с пересчетом к 15 градусам
+                //    //res = ef_dar.AddDailyAccountingReport();
+                //    //log_mes = String.Format("Сервис ReportTRKServices - Отработал метод AddDaily_Accounting_Report - Код выполнения:{0}", res);
+                //    //log_mes.SaveInformation();
+                //    //trk_log.AddTRKLogs(new TRKLogs()
+                //    //{
+                //    //    ID = 0,
+                //    //    DateTime = DateTime.Now,
+                //    //    Level = 4,
+                //    //    UserName = "ReportTRKServeces",
+                //    //    Log = log_mes
+                //    //});
+                //    //// Суточный детально по емкостям
+                //    //res = ef_adr.AddDailyAccountingDetaliReport();
+                //    //log_mes = String.Format("Сервис ReportTRKServices - Отработал метод AddDaily_Accounting_Detali_Report - Код выполнения:{0}", res);
+                //    //log_mes.SaveInformation();
+                //    //trk_log.AddTRKLogs(new TRKLogs()
+                //    //{
+                //    //    ID = 0,
+                //    //    DateTime = DateTime.Now,
+                //    //    Level = 4,
+                //    //    UserName = "ReportTRKServeces",
+                //    //    Log = log_mes
+                //    //});
 
-                    //// Суточный с переносом в ЦОД
-                    //int res_rt = tr.RemainsTanksToDC();
-                    //int res_rect = tr.ReceivingTanksToDC();
-                    //int res_dt = tr.DeliveryTanksToDC();
-                    //int res_radr = tr.Daily_Accounting_Detali_ReportToDC();
-                    //int res_dar = tr.Daily_Accounting_ReportToDC();
+                //    //// Суточный с переносом в ЦОД
+                //    //int res_rt = tr.RemainsTanksToDC();
+                //    //int res_rect = tr.ReceivingTanksToDC();
+                //    //int res_dt = tr.DeliveryTanksToDC();
+                //    //int res_radr = tr.Daily_Accounting_Detali_ReportToDC();
+                //    //int res_dar = tr.Daily_Accounting_ReportToDC();
 
-                    //log_mes = String.Format("Сервис ReportTRKServices - Отработали методы переноса в ЦОД - Коды выполнения RemainsTanksToDC:{0}, ReceivingTanksToDC:{1}, DeliveryTanksToDC:{2}, Daily_Accounting_Detali_ReportToDC:{3}, Daily_Accounting_ReportToDC:{4}",
-                    //    res_rt, res_rect, res_dt, res_radr, res_dar);
-                    //log_mes.SaveInformation();
-                    //trk_log.AddTRKLogs(new TRKLogs()
-                    //{
-                    //    ID = 0,
-                    //    DateTime = DateTime.Now,
-                    //    Level = 4,
-                    //    UserName = "ReportTRKServeces",
-                    //    Log = log_mes
-                    //});
-                    //// Чистка базы
-                    //res = ef_tl.Delete_Tanks();
-                    //log_mes = String.Format("Сервис ReportTRKServices - Отработал метод Delete_Tanks - Код выполнения:{0}", res);
-                    //log_mes.SaveInformation();
-                    //trk_log.AddTRKLogs(new TRKLogs()
-                    //{
-                    //    ID = 0,
-                    //    DateTime = DateTime.Now,
-                    //    Level = 4,
-                    //    UserName = "ReportTRKServeces",
-                    //    Log = log_mes
-                    //});
-                    time_daily = true;
-                }
+                //    //log_mes = String.Format("Сервис ReportTRKServices - Отработали методы переноса в ЦОД - Коды выполнения RemainsTanksToDC:{0}, ReceivingTanksToDC:{1}, DeliveryTanksToDC:{2}, Daily_Accounting_Detali_ReportToDC:{3}, Daily_Accounting_ReportToDC:{4}",
+                //    //    res_rt, res_rect, res_dt, res_radr, res_dar);
+                //    //log_mes.SaveInformation();
+                //    //trk_log.AddTRKLogs(new TRKLogs()
+                //    //{
+                //    //    ID = 0,
+                //    //    DateTime = DateTime.Now,
+                //    //    Level = 4,
+                //    //    UserName = "ReportTRKServeces",
+                //    //    Log = log_mes
+                //    //});
+                //    //// Чистка базы
+                //    //res = ef_tl.Delete_Tanks();
+                //    //log_mes = String.Format("Сервис ReportTRKServices - Отработал метод Delete_Tanks - Код выполнения:{0}", res);
+                //    //log_mes.SaveInformation();
+                //    //trk_log.AddTRKLogs(new TRKLogs()
+                //    //{
+                //    //    ID = 0,
+                //    //    DateTime = DateTime.Now,
+                //    //    Level = 4,
+                //    //    UserName = "ReportTRKServeces",
+                //    //    Log = log_mes
+                //    //});
+                //    time_daily = true;
+                //}
                 //if (h == 6 && (m >= 58 && m <= 59) && !time_sm_day)
                 //{
                 //    log_mes = String.Format("Сервис ReportTRKServices - сработал таймер на 7 часов");
@@ -222,14 +223,6 @@ namespace ReportBenzeneServices
             {
                 string log_mes = String.Format("OnTimerServices(sender={0}, args={1})", sender, args.ToString());
                 log_mes.SaveError(e);
-                //trk_log.AddTRKLogs(new TRKLogs()
-                //{
-                //    ID = 0,
-                //    DateTime = DateTime.Now,
-                //    Level = 2,
-                //    UserName = "ReportTRKServeces",
-                //    Log = log_mes
-                //});
             }
         }
     }
