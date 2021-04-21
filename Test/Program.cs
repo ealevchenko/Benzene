f5b98ce508bca5747a7785522312d7aa5f9cb3cb
Levchenko <lev75.eduard@gmail.com>
2021-04-21 22:57:15 +0300

    add test

---
+++ Test/Program.cs
@@ -0,0 +1,26 @@
+﻿using EFBenzene.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EFWEBDbContext ef_web = new EFWEBDbContext();
+            try
+            {
+                string sql = "EXEC [dbo].[Transfer_Daily_Report_Benzene]";
+                int res = ef_web.Database.ExecuteSqlCommand(sql);
+            }
+            catch (Exception e)
+            {
+                //String.Format("Ошибка выполнения метода Transfer_Daily_Report_Benzene()").SaveError(e);
+            }
+        }
+    }
+}
